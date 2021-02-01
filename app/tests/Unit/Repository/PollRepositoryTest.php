7c0b297fceba5478506db6e8f50a0f00fd99ed33
Cedric Anthony <git@antho.dev>
2021-02-01 12:05:59 +0100

    phpunit ok for poll repository

--- app/tests/Unit/Repository/PollRepositoryTest.php
+++ app/tests/Unit/Repository/PollRepositoryTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Repository;
 
+use App\Entity\Answer;
 use App\Entity\Poll;
+use App\Entity\User;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger as DoctrineOrmPurger;
 use Doctrine\Persistence\ManagerRegistry;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
@@ -39,16 +41,19 @@ class PollRepositoryTest extends KernelTestCase
         $this->assertEquals(10, count($polls));
     }
 
-    public function testFindAdminPoll(): void
+    public function testFindPollTest(): void
     {
         $this->loadDb();
 
-        // $adminPoll = $this->doctrine->getRepository(Poll::class)->findOneBy(['pollname' => 'admin']);
+        $pollTest = $this->doctrine->getRepository(Poll::class)->findOneBy(['title' => 'Do my test poll works?']);
 
-        // $this->assertSame('admin@noreply.local', $adminPoll->getEmail());
-        // $this->assertSame('test1234', $adminPoll->getPassword());
-        // $this->assertSame('ROLE_ADMIN', $adminPoll->getRole()->getCode());
-        // $this->assertEquals(new \DateTime() instanceof \DateTime, $adminPoll->getCreatedAt() instanceof \DateTime);
+        $this->assertSame('This is just a test to see if the poll is inserted in the db', $pollTest->getQuestion());
+        $this->assertEquals(new \DateTime() instanceof \DateTime, $pollTest->getCreatedAt() instanceof \DateTime);
+        $this->assertNotFalse($pollTest->getDoUsersMustBeConnected());
+        $this->assertSame('admin', $pollTest->getOwner()->getUsername());
+        $this->assertContainsOnlyInstancesOf(User::class, $pollTest->getParticipants());
+        $this->assertContainsOnlyInstancesOf(Answer::class, $pollTest->getAnswers());
+        $this->assertSame(42, $pollTest->getTotalVotes());
 
         $this->tearDown();
     }
