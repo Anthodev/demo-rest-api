7f0d749454bd0bf7becd74a6ab89873c19a44498
Cedric Anthony <git@antho.dev>
2021-02-01 19:14:03 +0100

    Fixed functions type (coding style)

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -271,7 +271,7 @@ class User implements UserInterface, \Serializable
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
         $this->plainPassword = null;
