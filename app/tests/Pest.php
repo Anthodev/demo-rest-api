9f3d236a37e20af6806c24be1138af79c7409c68
Cedric Anthony <git@antho.dev>
2021-02-11 19:01:26 +0100

    Converted remaining tests into pest tests + created custom KernelTestCase for pest to automatically reset the database between each test

--- app/tests/Pest.php
+++ app/tests/Pest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use App\Tests\Helpers\PasswordSameAssertion;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /*
 |--------------------------------------------------------------------------
